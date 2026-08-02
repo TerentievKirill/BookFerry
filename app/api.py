f7e6869b42a53c0c0d252beb1ffed2c8678b3873
Kirill Terentiev <kirillterentiev@gmail.com>
2026-08-02 20:47:15 +0500

    server 0.9_2

--- app/api.py
+++ app/api.py
@@ -4,6 +4,7 @@ from typing import List
 from app.models import  SearchRequest, SendBookRequest
 from app.services.opds import search_opds, Book
 from app.services.book_delivery import deliver_book
+from app.config import DEFAULT_OPDS_URL
 
 
 router = APIRouter()
