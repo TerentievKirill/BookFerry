17769bf9b8bf5048763ab69eb3c4baad631cd6b4
Kirill Terentiev <kirillterentiev@gmail.com>
2026-08-05 00:10:48 +0500

    server 1.0

--- app/services/opds.py
+++ app/services/opds.py
@@ -10,20 +10,30 @@ def search_opds(
     query: str,
 ) -> list[Book]:
     books = []
-
     search_url = f"{url.rstrip('/')}/search"
 
     for page_number in range(10):
-        response = requests.get(
-            search_url,
-            params={
-                "searchType": "books",
-                "searchTerm": query,
-                "pageNumber": page_number,
-            },
-            timeout=15,
-        )
-        response.raise_for_status()
+        try:
+            response = requests.get(
+                search_url,
+                params={
+                    "searchType": "books",
+                    "searchTerm": query,
+                    "pageNumber": page_number,
+                },
+                timeout=15,
+            )
+            response.raise_for_status()
+
+        except requests.RequestException:
+            # Если предыдущие страницы уже загрузились,
+            # возвращаем найденные книги.
+            if books:
+                break
+
+            # Если не загрузилась даже первая страница,
+            # передаём ошибку обработчику API.
+            raise
 
         root = etree.fromstring(response.content)
 
@@ -31,6 +41,7 @@ def search_opds(
             '//*[local-name()="entry"]'
         )
 
+        # Следующих страниц с книгами уже нет.
         if not entries:
             break
 
