6f8b585354b8c58a726419a5d89b9ff7b4ddc8c0
TerentievKirill <kirillterentiev@gmail.com>
2026-08-09 15:29:38 +0500

    Use unified API router

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from app.logging_config import (
     configure_logging,
     request_logging_middleware,
 )
-from app.pocketbook_api import router as pocketbook_router
 
 
 configure_logging()
@@ -18,4 +17,3 @@ init_catalog_db()
 app = FastAPI(title="BookFerry")
 app.middleware("http")(request_logging_middleware)
 app.include_router(router)
-app.include_router(pocketbook_router)
