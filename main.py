fc3ebc092701aecbd45db7d9582d7d3d1f79c30d
TerentievKirill <kirillterentiev@gmail.com>
2026-08-10 20:41:11 +0500

    Revert temporary download router wiring

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from app.api import router
 from app.db.catalog_database import init_catalog_db
 from app.db.database import init_db
-from app.download_client_result import router as download_client_result_router
 from app.logging_config import (
     configure_logging,
     request_logging_middleware,
@@ -18,4 +17,3 @@ init_catalog_db()
 app = FastAPI(title="BookFerry")
 app.middleware("http")(request_logging_middleware)
 app.include_router(router)
-app.include_router(download_client_result_router)
