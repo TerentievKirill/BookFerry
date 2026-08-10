05659e74d4ea18c55c35821563cc106b92544773
TerentievKirill <kirillterentiev@gmail.com>
2026-08-10 15:19:13 +0500

    Send email attachments from memory

--- app/services/mail.py
+++ app/services/mail.py
@@ -1,4 +1,3 @@
-import os
 import smtplib
 from email.message import EmailMessage
 
@@ -13,22 +12,20 @@ from app.config import (
 
 def send_file(
     recipient_email: str,
-    file_path: str,
+    file_content: bytes,
+    filename: str,
     subject: str | None = None,
 ):
-    with open(file_path, "rb") as file:
-        file_data = file.read()
-
     msg = EmailMessage()
     msg["Subject"] = subject or DEFAULT_SUBJECT
     msg["From"] = SMTP_LOGIN
     msg["To"] = recipient_email
     msg.set_content("")
     msg.add_attachment(
-        file_data,
+        file_content,
         maintype="application",
         subtype="octet-stream",
-        filename=os.path.basename(file_path),
+        filename=filename,
     )
 
     with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
