5740ef65a17bdf58e1c4b929ce8d1e9e70286d8c
TerentievKirill <kirillterentiev@gmail.com>
2026-08-18 13:58:58 +0500

    Update test_book_flow.py

--- tests/smoke/test_book_flow.py
+++ tests/smoke/test_book_flow.py
@@ -11,14 +11,30 @@ def test_pocketbook_user_can_find_book(flow):
     assert book.title
     assert book.url
 
-def test_telegram_user_can_find_book(flow):
-    user = flow.create_configured_telegram_user(
-        telegram_id=0,
+def test_pocketbook_user_can_find_book(flow):
+    user = flow.create_configured_pocketbook_user(
         catalog_id=3,
     )
 
     book = flow.find_first_book(
         uid=user.uid,
+        query="Лабиринт отражений",
+    )
+
+    assert book.title
+    assert book.url
+
+
+def test_telegram_user_can_find_book(flow):
+    telegram_id = 987654321
+
+    flow.create_configured_telegram_user(
+        telegram_id=telegram_id,
+        catalog_id=3,
+    )
+
+    book = flow.find_first_telegram_book(
+        telegram_id=telegram_id,
         query="Лабиринт отражений",
     )
 
